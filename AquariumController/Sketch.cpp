88a6dbc514f811d7bccd8f078827f813aa15f2b0
mloispro <olson_mitchell@hotmail.com>
2016-09-13 12:33:16 -0500

    high water float sensor

--- AquariumController/Sketch.cpp
+++ AquariumController/Sketch.cpp
@@ -37,7 +37,7 @@ int _doserFloatSwitchPin = A8;//MegaPins::A_8;
 int _tankPumpPin = 28;
 int _roPumpFloatSwitchPin = A9;//MegaPins::A_9;
 int _roPumpPin = 29;
-int _waterSensorPin = A10;//MegaPins::A_10;
+int _highWaterFloatSwitchPin = A10;//MegaPins::A_10;
 
 #pragma region DOSER
 
@@ -62,7 +62,7 @@ bool _tankPumpEnabled = false;
 Pump _tankPump;
 AnalogSwitch _roPumpFloatSwitch(_roPumpFloatSwitchPin);
 ROPump _roPump;
-AnalogSwitch _waterSensor(_waterSensorPin, 650); //todo: set max per tank.
+AnalogSwitch _highWaterFloatSwitch(_highWaterFloatSwitchPin, 900); //todo: set max per tank.
 
 #pragma endregion PUMP
 
@@ -138,7 +138,7 @@ void setup() {
 
     _tankPump = Pump(_tankPumpPin, 2, _tankPumpPin, 604800, _tankPumpEnabled); //weekly
     _tankPump.SetRelayHigh();
-    _roPump = ROPump(_roPumpPin, _roPumpFloatSwitchPin, _waterSensor, true);
+    _roPump = ROPump(_roPumpPin, _roPumpFloatSwitchPin, _highWaterFloatSwitchPin, true);
     _roPump.SetRelayHigh();
 
     //Motors.push_back(_doser);
